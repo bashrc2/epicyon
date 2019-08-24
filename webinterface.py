4921332f77f9ba40df9101a55880930a66341b0c
Bob Mottram <bob@freedombone.net>
2019-08-24 19:46:50 +0100

    Minimize javascript use

--- webinterface.py
+++ webinterface.py
@@ -992,24 +992,8 @@ def clickToDropDownScript() -> str:
     """Function run onclick to create a dropdown
     """
     script= \
-        '/* When the user clicks on the button,' \
-        'toggle between hiding and showing the dropdown content */' \
         'function dropdown() {' \
         '  document.getElementById("myDropdown").classList.toggle("show");' \
-        '}' \
-        '' \
-        '// Close the dropdown menu if the user clicks outside of it' \
-        'window.onclick = function(event) {' \
-        "  if (!event.target.matches('.dropdown')) {" \
-        '    var dropdowns = document.getElementsByClassName("dropdown-content");' \
-        '    var i;' \
-        '    for (i = 0; i < dropdowns.length; i++) {' \
-        '      var openDropdown = dropdowns[i];' \
-        "      if (openDropdown.classList.contains('show')) {" \
-        "        openDropdown.classList.remove('show');" \
-        '      }' \
-        '    }' \
-        '  }' \
         '}'
     return script
 
