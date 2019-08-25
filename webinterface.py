c2920629ed975e288d0b64f99e71b7a34c6b963f
Bob Mottram <bob@freedombone.net>
2019-08-25 14:36:43 +0100

    Remove non-working javascript

--- webinterface.py
+++ webinterface.py
@@ -991,19 +991,19 @@ def clickToDropDownScript() -> str:
     script= \
         'function dropdown() {' \
         '  document.getElementById("myDropdown").classList.toggle("show");' \
-        '}' \
-        'window.onclick = function(event) {' \
-        "  if (!event.target.matches('.dropbtn')) {" \
-        '    var dropdowns = document.getElementsByClassName("dropdown-content");' \
-        '    var i;' \
-        '    for (i = 0; i < dropdowns.length; i++) {' \
-        '      var openDropdown = dropdowns[i];' \
-        "      if (openDropdown.classList.contains('show')) {" \
-        "        openDropdown.classList.remove('show');" \
-        '      }' \
-        '    }' \
-        '  }' \
         '}'
+        #'window.onclick = function(event) {' \
+        #"  if (!event.target.matches('.dropbtn')) {" \
+        #'    var dropdowns = document.getElementsByClassName("dropdown-content");' \
+        #'    var i;' \
+        #'    for (i = 0; i < dropdowns.length; i++) {' \
+        #'      var openDropdown = dropdowns[i];' \
+        #"      if (openDropdown.classList.contains('show')) {" \
+        #"        openDropdown.classList.remove('show');" \
+        #'      }' \
+        #'    }' \
+        #'  }' \
+        #'}'
     return script
 
 def contentWarningScript() -> str:
