b70532d4da174adfcff5456f0975330ec0807d27
Bob Mottram <bob@freedombone.net>
2019-08-25 13:23:25 +0100

    Dropdown disappears when click away

--- webinterface.py
+++ webinterface.py
@@ -991,6 +991,18 @@ def clickToDropDownScript() -> str:
     script= \
         'function dropdown() {' \
         '  document.getElementById("myDropdown").classList.toggle("show");' \
+        '}' \
+        'window.onclick = function(event) {' \
+        "  if (!event.target.matches('.dropdown')) {" \
+        '    var dropdowns = document.getElementsByClassName("dropdown-content");' \
+        '    var i;' \
+        '    for (i = 0; i < dropdowns.length; i++) {' \
+        '      var openDropdown = dropdowns[i];' \
+        "      if (openDropdown.classList.contains('show')) {" \
+        "        openDropdown.classList.remove('show');" \
+        '      }' \
+        '    }' \
+        '  }' \
         '}'
     return script
 
