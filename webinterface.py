76c31b463ece2cc709996a1889f3f221d0eef10b
Bob Mottram <bob@freedombone.net>
2019-09-14 19:40:47 +0100

    Too many ors

--- webinterface.py
+++ webinterface.py
@@ -52,7 +52,7 @@ def updateAvatarImageCache(session,baseDir: str,httpPrefix: str,actor: str,avata
         sessionHeaders = {'Accept': 'image/png'}
         avatarImageFilename=baseDir+'/cache/avatars/'+actor.replace('/','-')+'.png'
     elif avatarUrl.endswith('.jpg') or avatarUrl.endswith('.jpeg') or \
-          or '.jpg?' in avatarUrl or '.jpeg?' in avatarUrl:
+         '.jpg?' in avatarUrl or '.jpeg?' in avatarUrl:
         sessionHeaders = {'Accept': 'image/jpeg'}
         avatarImageFilename=baseDir+'/cache/avatars/'+actor.replace('/','-')+'.jpg'
     elif avatarUrl.endswith('.gif') or '.gif?' in avatarUrl:
