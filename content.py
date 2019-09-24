7c497bb323f6945e2878a3efd5e16b746b8b8895
Bob Mottram <bob@freedombone.net>
2019-09-24 10:27:34 +0100

    Add music emoji to music links

--- content.py
+++ content.py
@@ -25,7 +25,7 @@ def addMusicTag(content: str,tag: str) -> str:
             break
     if not musicSiteFound:
         return content
-    return content+' '+tag+' '
+    return ':music: '+content+' '+tag+' '
 
 def addWebLinks(content: str) -> str:
     """Adds markup for web links
