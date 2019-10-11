5f0416a73b5cd6e42bf13321a2c41a84adaf5b0d
Bob Mottram <bob@freedombone.net>
2019-10-11 12:55:15 +0100

    Remove debug

--- webinterface.py
+++ webinterface.py
@@ -2895,8 +2895,6 @@ def htmlCalendar(translate: {}, \
                     if currDate.month==monthNumber:
                         if dayOfMonth==currDate.day:
                             isToday=True
-                if dayOfMonth==4 or dayOfMonth==5 or dayOfMonth==12:
-                    events[str(dayOfMonth)]=[{"test": 1},{"test2": 2},{"test3": 3},{"test4": 4}]
                 if events.get(str(dayOfMonth)):
                     eventDots=''
                     if len(events[str(dayOfMonth)])>1:
