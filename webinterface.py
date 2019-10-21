02ea84eadc0bf3f5913b35cc0cddf69832f8e8fa
Bob Mottram <bob@freedombone.net>
2019-10-21 21:19:08 +0100

    Lower threshold

--- webinterface.py
+++ webinterface.py
@@ -2353,7 +2353,7 @@ def htmlTimeline(translate: {},pageNumber: int, \
             tlStr+='</div>\n'
 
     # page down arrow
-    if itemCtr>=itemsPerPage/2:
+    if itemCtr>1:
         tlStr+='<center><a href="'+actor+'/'+boxName+'?page='+str(pageNumber+1)+'"><img loading="lazy" class="pageicon" src="/'+iconsDir+'/pagedown.png" title="'+translate['Page down']+'" alt="'+translate['Page down']+'"></a></center>'
     tlStr+=htmlFooter()
     return tlStr
