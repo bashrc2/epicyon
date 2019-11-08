d1b0adbd862b9aae4f4c1effcc869caba48ecdce
Bob Mottram <bob@freedombone.net>
2019-11-08 10:21:26 +0000

    Debug on vote creation

--- daemon.py
+++ daemon.py
@@ -3568,6 +3568,8 @@ class PubServer(BaseHTTPRequestHandler):
                                     messageJson, \
                                     self.server.maxReplies, \
                                     self.server.debug)
+            else:
+                print('ERROR: unable to create vote')
             self._redirect_headers(actor+'/inbox?page='+str(pageNumber),cookie)
             self.server.POSTbusy=False
             return                
