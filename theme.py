a2d5c6dac6dd57e2576596e659669368e5bfb99e
Bob Mottram <bob@freedombone.net>
2019-11-23 13:12:02 +0000

    Monospace

--- theme.py
+++ theme.py
@@ -139,7 +139,7 @@ def setThemeHacker(baseDir: str):
         "button-text": "green",
         "button-corner-radius": "4px",
         "timeline-border-radius": "4px",
-        "*font-family": "'Courier'",
+        "*font-family": "'monospace'",
         "background-color: #554;": "background-color: #062200;",
         "color: #FFFFFE;": "color: green;",
         "calendar-bg-color": "black",
