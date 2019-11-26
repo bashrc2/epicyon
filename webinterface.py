252c14211049268d881d619bd9db1d01f6423e8c
Bob Mottram <bob@freedombone.net>
2019-11-26 12:26:59 +0000

    Title for answers

--- webinterface.py
+++ webinterface.py
@@ -980,6 +980,7 @@ def htmlNewPost(translate: {},baseDir: str, \
         placeholderMessage=translate['Enter your question']+'...'
         endpoint='newquestion'
         extraFields='<div class="container">'
+        extraFields+='  <label class="labels">'+translate['Possible answers']+':</label>'
         for questionCtr in range(8):
             extraFields+='  <input type="text" class="questionOption" placeholder="'+str(questionCtr+1)+'" name="questionOption'+str(questionCtr)+'">'
         extraFields+='  <label class="labels">'+translate['Duration of listing in days']+':</label> <input type="number" name="duration" min="1" max="365" step="1" value="14">'
