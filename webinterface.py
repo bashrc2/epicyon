837fa46bdd4a4dae304403a4029e9fb9cef936cb
Bob Mottram <bob@freedombone.net>
2019-11-27 11:00:09 +0000

    Newline

--- webinterface.py
+++ webinterface.py
@@ -2639,7 +2639,7 @@ def htmlTimeline(recentPostsCache: {},maxRecentPosts: int,
         tlStr+='<form method="POST" action="/users/'+nickname+'/moderationaction">'
         tlStr+='<div class="container">\n'
         tlStr+='    <b>'+translate['Nickname or URL. Block using *@domain or nickname@domain']+'</b><br>\n'
-        tlStr+='    <input type="text" placeholder="" name="moderationAction" value="" autofocus>'
+        tlStr+='    <input type="text" placeholder="" name="moderationAction" value="" autofocus><br>\n'
         tlStr+='    <input type="submit" title="'+translate['Remove the above item']+'" name="submitRemove" value="'+translate['Remove']+'">'
         tlStr+='    <input type="submit" title="'+translate['Suspend the above account nickname']+'" name="submitSuspend" value="'+translate['Suspend']+'">'
         tlStr+='    <input type="submit" title="'+translate['Remove a suspension for an account nickname']+'" name="submitUnsuspend" value="'+translate['Unsuspend']+'">'
