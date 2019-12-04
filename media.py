92941ac85746c56a5b3eab97aad933cc7ea6f60d
Bob Mottram <bob@freedombone.net>
2019-12-04 16:20:54 +0000

    Attachment type

--- media.py
+++ media.py
@@ -115,7 +115,7 @@ def attachMedia(baseDir: str,httpPrefix: str,domain: str,port: int, \
     attachmentJson={
         'mediaType': mediaType,
         'name': description,
-        'type': 'Document',
+        'type': mediaType.split('/')[0],
         'url': httpPrefix+'://'+domain+'/'+mediaPath
     }
     if useBlurhash and mediaType.startswith('image/'):
