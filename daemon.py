0277b309d9d9075f967c30519b2374344a32eca7
Bob Mottram <bob@freedombone.net>
2020-02-19 11:37:33 +0000

    Check if port is already used

--- daemon.py
+++ daemon.py
@@ -147,6 +147,7 @@ from shares import outboxUndoShareUpload
 from shares import addShare
 from shares import removeShare
 from shares import expireShares
+from utils import isPortInUse
 from utils import removeAvatarFromCache
 from utils import locatePost
 from utils import getCachedPostFilename
@@ -5481,6 +5482,11 @@ def runDaemon(mediaInstance: bool,maxRecentPosts: int, \
         pubHandler = partial(PubServerUnitTest)
     else:
         pubHandler = partial(PubServer)
+
+    if isPortInUse(proxyPort):
+        print('Process is already running on port '+str(proxyPort))
+        return False
+
     try:
         httpd = ThreadingHTTPServer(serverAddress, pubHandler)
     except Exception as e:
