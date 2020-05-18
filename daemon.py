3badf3ad579a18eeffb8d7798f1a30797fb0c75e
Bob Mottram <bob@freedombone.net>
2020-05-18 13:04:05 +0100

    Test authorized state

--- daemon.py
+++ daemon.py
@@ -1295,7 +1295,7 @@ class PubServer(BaseHTTPRequestHandler):
             if blogFilename and nickname:
                 postJsonObject = loadJson(blogFilename)
                 if isBlogPost(postJsonObject):
-                    msg = htmlBlogPost(authorized,
+                    msg = htmlBlogPost(True,
                                        self.server.baseDir,
                                        self.server.httpPrefix,
                                        self.server.translate,
