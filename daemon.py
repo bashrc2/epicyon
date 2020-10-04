b58298d8aaa261440e26c8e4a4fd2d82d26c43d7
Bob Mottram <bob@freedombone.net>
2020-10-04 22:07:44 +0100

    Fix condition

--- daemon.py
+++ daemon.py
@@ -1068,7 +1068,7 @@ class PubServer(BaseHTTPRequestHandler):
 
         if self.path.startswith('/icons/') or \
            self.path.startswith('/avatars/') or \
-           self.path.startswith('/favicon.ico'):
+           self.path.startswith('/favicon.ico') or \
            self.path.startswith('/newswire.xml'):
             return False
 
