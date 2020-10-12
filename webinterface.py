ad29df38f5860a42b75fb640c55fab99e303319f
Bob Mottram <bob@freedombone.net>
2020-10-12 21:21:19 +0100

    Links textarea height

--- webinterface.py
+++ webinterface.py
@@ -1272,7 +1272,7 @@ def htmlEditLinks(translate: {}, baseDir: str, path: str,
         translate['One link per line. Description followed by the link.'] + \
         '<br>'
     editLinksForm += \
-        '  <textarea id="message" name="editedLinks" style="height:50%">' + \
+        '  <textarea id="message" name="editedLinks" style="height:50vh">' + \
         linksStr + '</textarea>'
     editLinksForm += \
         '</div>'
