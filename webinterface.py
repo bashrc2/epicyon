719ec924130408bce04fbbcc8240e0bc2441eb2b
Bob Mottram <bob@freedombone.net>
2020-10-20 12:55:49 +0100

    Check date format

--- webinterface.py
+++ webinterface.py
@@ -5631,6 +5631,8 @@ def htmlNewswire(newswire: {}, nickname: str, moderator: bool,
     """
     htmlStr = ''
     for dateStr, item in newswire.items():
+        if '+00:00' not in dateStr:
+            continue
         publishedDate = \
             datetime.strptime(dateStr, "%Y-%m-%d %H:%M:%S+00:00")
         dateShown = publishedDate.strftime("%Y-%m-%d %H:%M")
