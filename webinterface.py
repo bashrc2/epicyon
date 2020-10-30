ced8237af799ffd637f2b6bbac16388a6e12aaf1
Bob Mottram <bob@freedombone.net>
2020-10-30 18:51:59 +0000

    Icons on right

--- webinterface.py
+++ webinterface.py
@@ -5592,7 +5592,7 @@ def getLeftColumnContent(baseDir: str, nickname: str, domainFull: str,
     if ':' in domain:
         domain = domain.split(':')
 
-    editImageClass = 'leftColEdit'
+    editImageClass = ''
     if showHeaderImage:
         leftColumnImageFilename = \
             baseDir + '/accounts/' + nickname + '@' + domain + \
@@ -5610,6 +5610,7 @@ def getLeftColumnContent(baseDir: str, nickname: str, domainFull: str,
                          leftColumnImageFilename)
 
         # show the image at the top of the column
+        editImageClass = 'leftColEdit'
         if os.path.isfile(leftColumnImageFilename):
             editImageClass = 'leftColEditImage'
             htmlStr += \
