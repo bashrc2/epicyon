e38e4a11d918c7480a4509aee8de0763b82a561a
Bob Mottram <bob@freedombone.net>
2020-11-02 20:17:20 +0000

    Same banner on search

--- theme.py
+++ theme.py
@@ -1019,6 +1019,8 @@ def setThemeIndymediaModern(baseDir: str):
     fontStrItalic = \
         "url('./fonts/NimbusSanL-italic.otf') format('opentype')"
     themeParams = {
+        "search-banner-height": "15vh",
+        "search-banner-height-mobile": "10vh",
         "publish-button-vertical-offset": "10px",
         "container-button-padding": "0px",
         "container-button-margin": "0px",
