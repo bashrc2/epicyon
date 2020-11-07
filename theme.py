880902460b8d5a238a67581c9c7c408a02522fee
Bob Mottram <bob@freedombone.net>
2020-11-07 21:32:52 +0000

    Closer icons

--- theme.py
+++ theme.py
@@ -1026,6 +1026,7 @@ def setThemeIndymediaModern(baseDir: str):
     fontStrItalic = \
         "url('./fonts/NimbusSanL-italic.otf') format('opentype')"
     themeParams = {
+        "containericons-horizontal-spacing": "0%",
         "font-size-header": "14px",
         "font-size": "22px",
         "font-size2": "16px",
