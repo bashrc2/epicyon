d6b22535f75a5b334c5a988c988982d16b722af3
Bob Mottram <bob@freedombone.net>
2020-11-08 16:52:57 +0000

    Comments

--- newsdaemon.py
+++ newsdaemon.py
@@ -663,9 +663,11 @@ def convertRSStoActivityPub(baseDir: str, httpPrefix: str,
                     if os.path.isfile(filename + '.arrived'):
                         os.remove(filename + '.arrived')
 
-                # set the url
+                # setting the url here links to the activitypub object
+                # stored locally
                 # newswire[originalDateStr][1] = \
                 #     '/users/news/statuses/' + statusNumber
+
                 # set the filename
                 newswire[originalDateStr][3] = filename
 
