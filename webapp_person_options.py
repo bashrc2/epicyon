0f2f21359eadbc5986aa7a5c72d9f6389a17ca29
Bob Mottram <bob@freedombone.net>
2020-11-10 20:40:19 +0000

    Back button path

--- webapp_person_options.py
+++ webapp_person_options.py
@@ -202,9 +202,13 @@ def htmlPersonOptions(cssCache: {}, translate: {}, baseDir: str,
             optionsStr += checkboxStr
 
     optionsStr += optionsLinkStr
+    backPath = '/'
+    if nickname:
+        backPath = '/users/' + nickname
     optionsStr += \
-        '    <a href="/"><button type="button" class="buttonIcon" ' + \
-        'name="submitBack">' + translate['Go Back'] + '</button></a>'
+        '    <a href="' + backPath + '"><button type="button" ' + \
+        'class="buttonIcon" name="submitBack">' + translate['Go Back'] + \
+        '</button></a>'
     optionsStr += \
         '    <button type="submit" class="button" name="submitView">' + \
         translate['View'] + '</button>'
