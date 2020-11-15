29676ddbdd153d80eec47e4359c2a658de1e4742
Bob Mottram <bob@freedombone.net>
2020-11-15 20:56:22 +0000

    Postning new items from the shares timeline

--- webapp_timeline.py
+++ webapp_timeline.py
@@ -312,6 +312,20 @@ def htmlTimeline(cssCache: {}, defaultTimeline: str,
                 '<a href="' + usersPath + '/newevent">' + \
                 '<button class="button"><span>' + \
                 translate['Post'] + '</span></button></a>'
+    elif boxName == 'tlshares':
+        if not iconsAsButtons:
+            newPostButtonStr += \
+                '<a class="imageAnchor" href="' + usersPath + \
+                '/newshare"><img loading="lazy" src="/' + \
+                iconsPath + '/newpost.png" title="' + \
+                translate['Create a new shared item'] + '" alt="| ' + \
+                translate['Create a new shared item'] + \
+                '" class="timelineicon"/></a>\n'
+        else:
+            newPostButtonStr += \
+                '<a href="' + usersPath + '/newshare">' + \
+                '<button class="button"><span>' + \
+                translate['Post'] + '</span></button></a>'
     else:
         if not manuallyApproveFollowers:
             if not iconsAsButtons:
