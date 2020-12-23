d7da2f2e65a52a9930f4a948ced7618ff13635db
Bob Mottram <bob@freedombone.net>
2020-12-23 22:50:21 +0000

    Add invidious sites

--- webapp_media.py
+++ webapp_media.py
@@ -40,6 +40,13 @@ def _addEmbeddedVideoFromSites(translate: {}, content: str,
             return content
 
     invidiousSites = ('https://invidious.snopyta.org',
+                      'https://yewtu.be',
+                      'https://tube.connect.cafe',
+                      'https://invidious.kavin.rocks',
+                      'https://invidiou.site',
+                      'https://invidious.tube',
+                      'https://invidious.xyz',
+                      'https://invidious.zapashcanon.fr',
                       'http://c7hqkpkpemu6e7emz5b4vy' +
                       'z7idjgdvgaaa3dyimmeojqbgpea3xqjoid.onion',
                       'http://axqzx4s6s54s32yentfqojs3x5i7faxza6xo3ehd4' +
