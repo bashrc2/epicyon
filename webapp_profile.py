51866a5fdfb3c5a1879699bb3758b5db2d0da8e3
Bob Mottram <bob@freedombone.net>
2021-01-05 10:52:37 +0000

    Check status for verifying signatures

--- webapp_profile.py
+++ webapp_profile.py
@@ -1106,10 +1106,16 @@ def htmlEditProfile(cssCache: {}, translate: {}, baseDir: str, path: str,
             instanceStr += \
                 '  <input type="file" id="instanceLogo" name="instanceLogo"'
             instanceStr += '      accept="' + imageFormats + '">'
-            instanceStr += \
-                '      <input type="checkbox" class="profilecheckbox" ' + \
-                'name="verifyallsignatures"> ' + \
-                translate['Verify all signatures'] + '<br>\n'
+            if getConfigParam(baseDir, "verifyAllSignatures"):
+                instanceStr += \
+                    '      <input type="checkbox" class="profilecheckbox" ' + \
+                    'name="verifyallsignatures" checked> ' + \
+                    translate['Verify all signatures'] + '<br>\n'
+            else:
+                instanceStr += \
+                    '      <input type="checkbox" class="profilecheckbox" ' + \
+                    'name="verifyallsignatures"> ' + \
+                    translate['Verify all signatures'] + '<br>\n'
             instanceStr += '</div>'
 
             moderators = ''
