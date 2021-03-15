28a30728cff071027b1eefcc87121c6d21988415
Bob Mottram <bob@freedombone.net>
2021-03-15 20:33:07 +0000

    Follow request prompt

--- notifications_client.py
+++ notifications_client.py
@@ -481,7 +481,18 @@ def _showLocalBox(notifyJson: {}, boxName: str,
     sayStr += \
         'Use the \33[3mnext\33[0m and \33[3mprev\33[0m commands to navigate.'
     _sayCommand(sayStr, sayStr, screenreader, systemLanguage, espeak)
-
+    if notifyJson:
+        if notifyJson.get('followRequestsList'):
+            if len(notifyJson['followRequestsList']) > 0:
+                print('')
+                sayStr = indent + 'You have a follow request from ' + \
+                    '\33[7m' + notifyJson['followRequestsList'][0] + '\33[0m'
+                _sayCommand(sayStr, sayStr, screenreader, systemLanguage,
+                            espeak)
+                sayStr = indent + 'Use the \33[3maccept\33[0m or ' + \
+                    '\33[3mreject\33[0m commands to respond.'
+                _sayCommand(sayStr, sayStr, screenreader, systemLanguage,
+                            espeak)
     print('')
 
 
