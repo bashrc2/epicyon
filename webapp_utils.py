18f0a82a4708ea9bd940862f016a03c8bc589527
Bob Mottram <bob@freedombone.net>
2021-05-17 13:08:13 +0100

    Remove mainEntity

--- webapp_utils.py
+++ webapp_utils.py
@@ -762,8 +762,6 @@ def htmlHeaderWithPersonMarkup(cssFilename: str, instanceTitle: str,
                     skillsMarkup += '            "name": "' + roleName + '",\n'
                     skillsMarkup += '            "description": ' + \
                         '"Fediverse instance role",\n'
-                    skillsMarkup += '            "mainEntityOfPage": "' + \
-                        actorJson['id'] + '",\n'
                     skillsMarkup += '            "occupationLocation": {\n'
                     skillsMarkup += \
                         '              "@type": "City",\n'
@@ -806,8 +804,6 @@ def htmlHeaderWithPersonMarkup(cssFilename: str, instanceTitle: str,
                     skillsMarkup += '          "name": "' + ocName + '",\n'
                     skillsMarkup += '          "description": ' + \
                         '"Fediverse instance occupation",\n'
-                    skillsMarkup += '          "mainEntityOfPage": "' + \
-                        actorJson['id'] + '",\n'
                     skillsMarkup += '          "occupationLocation": {\n'
                     skillsMarkup += '            "@type": "City",\n'
                     skillsMarkup += \
