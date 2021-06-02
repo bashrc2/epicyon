47fa228cf452efb42b45009c2818cc2a1699e6bf
Bob Mottram <bob@freedombone.net>
2021-06-02 23:07:29 +0100

    Outbox

--- metadata.py
+++ metadata.py
@@ -71,7 +71,7 @@ def _getStatusCount(baseDir: str) -> int:
                 continue
             if 'inbox@' in acct or 'news@' in acct:
                 continue
-            statusCtr += len(os.path.join(accountsDir, acct))
+            statusCtr += len(os.path.join(accountsDir, acct + '/outbox'))
         break
     return statusCtr
 
