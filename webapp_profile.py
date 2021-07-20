f3a4190ad065447321f4d86405a7b56b6f5ae69d
Bob Mottram <bob@freedombone.net>
2021-07-20 21:21:25 +0100

    Define graphics section of profile

--- webapp_profile.py
+++ webapp_profile.py
@@ -2060,6 +2060,7 @@ def htmlEditProfile(cssCache: {}, translate: {}, baseDir: str, path: str,
     roleAssignStr = ''
     peertubeStr = ''
     libretranslateStr = ''
+    graphicsStr = ''
 
     adminNickname = getConfigParam(baseDir, 'admin')
 
