2602aa5cd8ec683e5c79d6d517804ebbd8798c47
Bob Mottram <bob@freedombone.net>
2021-08-01 16:14:12 +0100

    Typo

--- webapp_timeline.py
+++ webapp_timeline.py
@@ -739,7 +739,7 @@ def htmlTimeline(cssCache: {}, defaultTimeline: str,
     itemCtr = 0
     if timelineJson:
         if not timelineJson.get('orderedItems'):
-            print('ERROR: no orderedItems in timeline for ' + boxName:)
+            print('ERROR: no orderedItems in timeline for ' + boxName)
             return ''
 
     if timelineJson:
