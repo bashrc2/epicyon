f7898d09b921c15c1f7dfc1aedee347e87422b64
Bob Mottram <bob@freedombone.net>
2021-09-01 16:04:08 +0100

    Differentiate from other debug

--- daemon.py
+++ daemon.py
@@ -11078,7 +11078,7 @@ class PubServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.headers.get('Signature'):
-            print('Signed GET: ' + str(self.headers))
+            print('Signed HTTP GET: ' + str(self.headers))
 
         callingDomain = self.server.domainFull
         if self.headers.get('Host'):
