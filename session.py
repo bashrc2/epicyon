9d0764f72550eccb88429398ba44a419c8ff582c
Bob Mottram <bob@freedombone.net>
2021-09-17 16:12:50 +0100

    Update diagrams

--- session.py
+++ session.py
@@ -5,7 +5,7 @@ __version__ = "1.2.0"
 __maintainer__ = "Bob Mottram"
 __email__ = "bob@libreserver.org"
 __status__ = "Production"
-__module_group__ = "Core"
+__module_group__ = "Session"
 
 import os
 import requests
