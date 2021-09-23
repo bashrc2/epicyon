2bd88b0b6037de97bee9dc2ee5871b4fb68d2f85
Bob Mottram <bob@freedombone.net>
2021-09-23 19:01:15 +0100

    Cache header

--- daemon.py
+++ daemon.py
@@ -732,7 +732,6 @@ class PubServer(BaseHTTPRequestHandler):
         self.send_header('Origin', self.server.domainFull)
         self.send_header('InstanceID', self.server.instanceId)
         self.send_header('X-Clacks-Overhead', 'GNU Natalie Nguyen')
-        self.send_header('Cache-Control', 'max-age=0')
         self.send_header('Cache-Control', 'public')
 
     def _set_headers(self, fileFormat: str, length: int, cookie: str,
