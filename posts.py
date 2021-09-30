b46342b4550aeff334131c45b07f4e8188cb9541
Bob Mottram <bob@freedombone.net>
2021-09-30 10:18:03 +0100

    Same context as mastodon

--- posts.py
+++ posts.py
@@ -1385,10 +1385,13 @@ def _createPostBase(baseDir: str, nickname: str, domain: str, port: int,
     postContext = [
         'https://www.w3.org/ns/activitystreams',
         {
-            'Hashtag': 'as:Hashtag',
-            'sensitive': 'as:sensitive',
-            'toot': 'http://joinmastodon.org/ns#',
-            'votersCount': 'toot:votersCount'
+            "ostatus": "http://ostatus.org#",
+            "atomUri": "ostatus:atomUri",
+            "inReplyToAtomUri": "ostatus:inReplyToAtomUri",
+            "conversation": "ostatus:conversation",
+            "sensitive": "as:sensitive",
+            "toot": "http://joinmastodon.org/ns#",
+            "votersCount": "toot:votersCount"
         }
     ]
 
