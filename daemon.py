ebac89fb82c52a4bfc75491c9d131e04a26e524f
Bob Mottram <bob@freedombone.net>
2021-10-19 21:37:27 +0100

    Debug

--- daemon.py
+++ daemon.py
@@ -12704,7 +12704,7 @@ class PubServer(BaseHTTPRequestHandler):
                 elif graph == 'get':
                     graph = '_GET'
                 watchPointsJson = sortedWatchPoints(self.server.fitness, graph)
-                print('watchPointsJson: ' + str())
+                print('watchPointsJson: ' + str(watchPointsJson))
                 msg = json.dumps(watchPointsJson,
                                  ensure_ascii=False).encode('utf-8')
                 msglen = len(msg)
