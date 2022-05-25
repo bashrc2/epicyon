39b1d1d8f4b40f04b3724ffd20e3524428ceb70e
Bob Mottram <bob@libreserver.org>
2022-05-25 11:17:44 +0100

    No tab order to columns

--- webapp_frontscreen.py
+++ webapp_frontscreen.py
@@ -158,7 +158,7 @@ def html_front_screen(signing_priv_key_pem: str,
         '  </colgroup>\n' + \
         '  <tbody>\n' + \
         '    <tr>\n' + \
-        '      <td valign="top" class="col-left" tabindex="2">\n'
+        '      <td valign="top" class="col-left" tabindex="-1">\n'
     profile_header_str += \
         get_left_column_content(base_dir, 'news', domain_full,
                                 http_prefix, translate,
@@ -168,7 +168,7 @@ def html_front_screen(signing_priv_key_pem: str,
                                 shared_items_federated_domains)
     profile_header_str += \
         '      </td>\n' + \
-        '      <td valign="top" class="col-center" tabindex="1">\n'
+        '      <td valign="top" class="col-center" tabindex="-1">\n'
 
     profile_str = profile_header_str
 
@@ -200,7 +200,7 @@ def html_front_screen(signing_priv_key_pem: str,
     # Footer which is only used for system accounts
     profile_footer_str = '      </td>\n'
     profile_footer_str += \
-        '      <td valign="top" class="col-right" tabindex="3">\n'
+        '      <td valign="top" class="col-right" tabindex="-1">\n'
     profile_footer_str += \
         get_right_column_content(base_dir, 'news', domain_full,
                                  http_prefix, translate,
