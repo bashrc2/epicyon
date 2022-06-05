c61f7e3e6d8bb7c26bba58f83f8fe78322d4e713
Bob Mottram <bob@libreserver.org>
2022-06-05 11:01:41 +0100

    Map emoji

--- webapp_create_post.py
+++ webapp_create_post.py
@@ -725,7 +725,7 @@ def html_new_post(css_cache: {}, media_instance: bool, translate: {},
                     str(maps_zoom) + ',normal'
         location_label_with_link = \
             '<a href="' + maps_url + '" ' + \
-            'rel="nofollow noopener noreferrer" target="_blank">' + \
+            'rel="nofollow noopener noreferrer" target="_blank">🗺️ ' + \
             translate['Location'] + '</a>'
         date_and_location += '<br><p>\n' + \
             edit_text_field(location_label_with_link, 'location', '',
