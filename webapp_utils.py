f41d52630b8a8f84415eb9440db9acc0855c6472
Bob Mottram <bob@libreserver.org>
2022-11-27 13:24:34 +0000

    Remove viewport

--- webapp_utils.py
+++ webapp_utils.py
@@ -717,7 +717,6 @@ def html_header_with_external_style(css_filename: str, instance_title: str,
         metadata + \
         '    <meta name="apple-mobile-web-app-status-bar-style" ' + \
         'content="' + pwa_theme_background_color + '">\n' + \
-        '    <meta name="viewport" content="initial-scale=1">\n' + \
         '    <title>' + instance_title + '</title>\n' + \
         '  </head>\n' + \
         '  <body>\n'
