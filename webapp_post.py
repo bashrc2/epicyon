1eb50a2ad2d76ae833ab9c18c5289a33e5d45d64
Bob Mottram <bob@libreserver.org>
2022-12-27 17:09:34 +0000

    Use different symbol for cc0 licenses

--- webapp_post.py
+++ webapp_post.py
@@ -1582,10 +1582,14 @@ def _get_footer_with_icons(show_icons: bool,
         footer_str += '        '
         if content_license_url:
             # show the CC symbol
+            copyright_symbol = '🅭 '
+            if '/zero/' in content_license_url:
+                copyright_symbol = '🄍 '
             footer_str += '<a href="' + \
                 content_license_url + '" class="' + \
                 time_class + '" tabindex="10">' + \
-                '<span itemprop="license">🅭 </span></a>'
+                '<span itemprop="license"> ' + \
+                copyright_symbol + ' </span></a>'
         # show the date
         date_link = '/users/' + nickname + '?convthread=' + \
             published_link.replace('/', '--')
