5257ba4f55b33c277f4a04b3f9a102e9f74b8e41
Bob Mottram <bob@libreserver.org>
2023-01-02 11:27:52 +0000

    Only remove last post if there are some quantity

--- webapp_timeline.py
+++ webapp_timeline.py
@@ -1073,7 +1073,8 @@ def html_timeline(default_timeline: str,
         # if showing the page down icon then remove the last item so that
         # firstpost does not overlap on the next timeline
         if last_item_str and first_post_id != last_post_id:
-            tl_str = tl_str.replace(last_item_str, '')
+            if item_ctr > items_per_page / 2:
+                tl_str = tl_str.replace(last_item_str, '')
         tl_str += text_mode_separator
         first_post = ''
         if last_post_id:
