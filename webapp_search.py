d9d4577c5bedf751a954fd55ed78a0c46627f6a9
Bob Mottram <bob@libreserver.org>
2023-01-05 23:15:40 +0000

    debug

--- webapp_search.py
+++ webapp_search.py
@@ -1079,6 +1079,7 @@ def html_hashtag_search_remote(nickname: str, domain: str, port: int,
     as_header = {
         'Accept': 'application/activity+json; profile="' + profile_str + '"'
     }
+    debug = True
     hashtag_json = \
         get_json(signing_priv_key_pem,
                  session, hashtag_url, as_header, None, debug,
