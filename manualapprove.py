322d06e26a9661a81df5f343274d689eaef7da55
Bob Mottram <bob@libreserver.org>
2023-05-10 10:08:50 +0100

    Accepting follow requests without a users path

--- manualapprove.py
+++ manualapprove.py
@@ -178,13 +178,19 @@ def manual_approve_follow_request(session, session_onion, session_i2p,
         req_domain = approve_handle.split('@')[1].strip()
         req_prefix = http_prefix + '://' + req_domain
         paths = get_user_paths()
-        for user_path in paths:
-            if req_prefix + user_path + req_nick in approve_follows_str:
-                exists = True
-                approve_handle_full = req_prefix + user_path + req_nick
-                if group_account:
-                    approve_handle_full = '!' + approve_handle_full
-                break
+        if req_prefix + '/' + req_nick in approve_follows_str:
+            exists = True
+            approve_handle_full = req_prefix + '/' + req_nick
+            if group_account:
+                approve_handle_full = '!' + approve_handle_full
+        if not exists:
+            for user_path in paths:
+                if req_prefix + user_path + req_nick in approve_follows_str:
+                    exists = True
+                    approve_handle_full = req_prefix + user_path + req_nick
+                    if group_account:
+                        approve_handle_full = '!' + approve_handle_full
+                    break
     if not exists:
         print('Manual follow accept: ' + approve_handle_full +
               ' not in requests file "' +
