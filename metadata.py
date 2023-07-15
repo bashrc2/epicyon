7c7b43a86a66675d534cf6bced91017d539da11f
Bob Mottram <bob@libreserver.org>
2023-07-15 19:44:42 +0100

    List of FEPs within nodeinfo

--- metadata.py
+++ metadata.py
@@ -85,7 +85,8 @@ def meta_data_node_info(base_dir: str,
         },
         'metadata': {
             'chat_enabled': False,
-            'postFormats': ['text/plain', 'text/html', 'text/markdown']
+            'postFormats': ['text/plain', 'text/html', 'text/markdown'],
+            'FEPs': ['c648', '521a', '8fcf', '4ccd', 'c118', 'fffd', '1970']
         },
         'version': '2.1'
     }
