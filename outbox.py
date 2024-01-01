bcaa5d83a227bfa36d729bfd9c6fd23271e8fb6a
Bob Mottram <bob@libreserver.org>
2024-01-01 10:43:20 +0000

    More phones

--- outbox.py
+++ outbox.py
@@ -444,7 +444,7 @@ def post_message_to_outbox(session, translate: {},
     permitted_outbox_types = (
         'Create', 'Announce', 'Like', 'EmojiReact', 'Follow', 'Undo',
         'Update', 'Add', 'Remove', 'Block', 'Delete', 'Skill', 'Ignore',
-        'Move'
+        'Move', 'Edition'
     )
     if message_json['type'] not in permitted_outbox_types:
         if debug:
