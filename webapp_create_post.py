ab054a78cfbd7bd143a356269568f5319abedd02
Bob Mottram <bob@libreserver.org>
2024-01-04 14:36:01 +0000

    Missing quote

--- webapp_create_post.py
+++ webapp_create_post.py
@@ -839,7 +839,7 @@ def html_new_post(edit_post_params: {},
         books_url = 'https://en.wikipedia.org/wiki/Lists_of_books'
         extra_fields += '<br>' + \
             edit_text_field('<a href="' + books_url +
-                            ' target="_blank" ' +
+                            '" target="_blank" ' +
                             'rel="nofollow noopener noreferrer">URL</a>:',
                             'bookurl', '', 'https://...', True)
         extra_fields += '<br>' + \
