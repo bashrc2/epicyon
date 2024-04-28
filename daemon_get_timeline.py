01fbd5bbabdcbd288175ccd11863c2e3f6bdcadf
Bob Mottram <bob@libreserver.org>
2024-04-28 15:50:01 +0100

    Tidying

--- daemon_get_timeline.py
+++ daemon_get_timeline.py
@@ -1540,13 +1540,53 @@ def show_mod_timeline(self, authorized: bool,
                       domain: str, port: int, getreq_start_time,
                       cookie: str, debug: str,
                       curr_session, ua_str: str,
-                      max_posts_in_feed: int) -> bool:
+                      max_posts_in_feed: int,
+                      recent_posts_cache: {},
+                      positive_voting: bool,
+                      voting_time_mins: int,
+                      full_width_tl_button_header: bool,
+                      access_keys: {},
+                      key_shortcuts: {},
+                      shared_items_federated_domains: [],
+                      twitter_replacement_domain: str,
+                      allow_local_network_access: bool,
+                      show_published_date_only: bool,
+                      account_timezone: {},
+                      bold_reading_nicknames: {},
+                      min_images_for_accounts: [],
+                      reverse_sequence_nicknames: [],
+                      default_timeline: str,
+                      max_recent_posts: int,
+                      translate: {},
+                      cached_webfingers: {},
+                      person_cache: {},
+                      project_version: str,
+                      yt_replace_domain: str,
+                      newswire: {},
+                      show_publish_as_icon: bool,
+                      icons_as_buttons: bool,
+                      rss_icon_at_top: bool,
+                      publish_button_at_top: bool,
+                      theme_name: str,
+                      peertube_instances: [],
+                      text_mode_banner: str,
+                      system_language: str,
+                      max_like_count: int,
+                      signing_priv_key_pem: str,
+                      cw_lists: {},
+                      lists_enabled: {},
+                      dogwhistles: {},
+                      buy_sites: [],
+                      auto_cw_cache: {},
+                      fitness: {},
+                      onion_domain: str,
+                      i2p_domain: str) -> bool:
     """Shows the moderation timeline
     """
     if '/users/' in path:
         if authorized:
             moderation_feed = \
-                person_box_json(self.server.recent_posts_cache,
+                person_box_json(recent_posts_cache,
                                 base_dir,
                                 domain,
                                 port,
@@ -1554,8 +1594,8 @@ def show_mod_timeline(self, authorized: bool,
                                 http_prefix,
                                 max_posts_in_feed, 'moderation',
                                 True,
-                                0, self.server.positive_voting,
-                                self.server.voting_time_mins)
+                                0, positive_voting,
+                                voting_time_mins)
             if moderation_feed:
                 if request_http(self.headers, debug):
                     nickname = path.replace('/users/', '')
@@ -1575,7 +1615,7 @@ def show_mod_timeline(self, authorized: bool,
                     if 'page=' not in path:
                         # if no page was specified then show the first
                         moderation_feed = \
-                            person_box_json(self.server.recent_posts_cache,
+                            person_box_json(recent_posts_cache,
                                             base_dir,
                                             domain,
                                             port,
@@ -1584,97 +1624,79 @@ def show_mod_timeline(self, authorized: bool,
                                             max_posts_in_feed,
                                             'moderation',
                                             True,
-                                            0, self.server.positive_voting,
-                                            self.server.voting_time_mins)
-                    full_width_tl_button_header = \
-                        self.server.full_width_tl_button_header
+                                            0, positive_voting,
+                                            voting_time_mins)
                     moderation_action_str = ''
 
-                    access_keys = self.server.access_keys
-                    if self.server.key_shortcuts.get(nickname):
-                        access_keys = \
-                            self.server.key_shortcuts[nickname]
+                    if key_shortcuts.get(nickname):
+                        access_keys = key_shortcuts[nickname]
 
-                    shared_items_federated_domains = \
-                        self.server.shared_items_federated_domains
-                    twitter_replacement_domain = \
-                        self.server.twitter_replacement_domain
-                    allow_local_network_access = \
-                        self.server.allow_local_network_access
-                    show_published_date_only = \
-                        self.server.show_published_date_only
                     timezone = None
-                    if self.server.account_timezone.get(nickname):
-                        timezone = \
-                            self.server.account_timezone.get(nickname)
+                    if account_timezone.get(nickname):
+                        timezone = account_timezone.get(nickname)
                     bold_reading = False
-                    if self.server.bold_reading.get(nickname):
+                    if bold_reading_nicknames.get(nickname):
                         bold_reading = True
-                    min_images_for_accounts = \
-                        self.server.min_images_for_accounts
                     reverse_sequence = False
-                    if nickname in self.server.reverse_sequence:
+                    if nickname in reverse_sequence_nicknames:
                         reverse_sequence = True
                     msg = \
-                        html_moderation(self.server.default_timeline,
-                                        self.server.recent_posts_cache,
-                                        self.server.max_recent_posts,
-                                        self.server.translate,
+                        html_moderation(default_timeline,
+                                        recent_posts_cache,
+                                        max_recent_posts,
+                                        translate,
                                         page_number, max_posts_in_feed,
                                         curr_session,
                                         base_dir,
-                                        self.server.cached_webfingers,
-                                        self.server.person_cache,
+                                        cached_webfingers,
+                                        person_cache,
                                         nickname,
                                         domain,
                                         port,
                                         moderation_feed,
                                         True,
                                         http_prefix,
-                                        self.server.project_version,
-                                        self.server.yt_replace_domain,
+                                        project_version,
+                                        yt_replace_domain,
                                         twitter_replacement_domain,
                                         show_published_date_only,
-                                        self.server.newswire,
-                                        self.server.positive_voting,
-                                        self.server.show_publish_as_icon,
+                                        newswire,
+                                        positive_voting,
+                                        show_publish_as_icon,
                                         full_width_tl_button_header,
-                                        self.server.icons_as_buttons,
-                                        self.server.rss_icon_at_top,
-                                        self.server.publish_button_at_top,
+                                        icons_as_buttons,
+                                        rss_icon_at_top,
+                                        publish_button_at_top,
                                         authorized, moderation_action_str,
-                                        self.server.theme_name,
-                                        self.server.peertube_instances,
+                                        theme_name,
+                                        peertube_instances,
                                         allow_local_network_access,
-                                        self.server.text_mode_banner,
+                                        text_mode_banner,
                                         access_keys,
-                                        self.server.system_language,
-                                        self.server.max_like_count,
+                                        system_language,
+                                        max_like_count,
                                         shared_items_federated_domains,
-                                        self.server.signing_priv_key_pem,
-                                        self.server.cw_lists,
-                                        self.server.lists_enabled,
+                                        signing_priv_key_pem,
+                                        cw_lists,
+                                        lists_enabled,
                                         timezone, bold_reading,
-                                        self.server.dogwhistles,
+                                        dogwhistles,
                                         ua_str,
                                         min_images_for_accounts,
                                         reverse_sequence,
-                                        self.server.buy_sites,
-                                        self.server.auto_cw_cache)
+                                        buy_sites,
+                                        auto_cw_cache)
                     msg = msg.encode('utf-8')
                     msglen = len(msg)
                     set_headers(self, 'text/html', msglen,
                                 cookie, calling_domain, False)
                     write2(self, msg)
-                    fitness_performance(getreq_start_time,
-                                        self.server.fitness,
+                    fitness_performance(getreq_start_time, fitness,
                                         '_GET', '_show_mod_timeline',
                                         debug)
                 else:
                     # don't need authorized fetch here because
                     # there is already the authorization check
-                    onion_domain = self.server.onion_domain
-                    i2p_domain = self.server.i2p_domain
                     msg_str = json.dumps(moderation_feed,
                                          ensure_ascii=False)
                     msg_str = convert_domains(calling_domain,
@@ -1691,8 +1713,7 @@ def show_mod_timeline(self, authorized: bool,
                     set_headers(self, protocol_str, msglen,
                                 None, calling_domain, False)
                     write2(self, msg)
-                    fitness_performance(getreq_start_time,
-                                        self.server.fitness,
+                    fitness_performance(getreq_start_time, fitness,
                                         '_GET', '_show_mod_timeline json',
                                         debug)
                 return True
