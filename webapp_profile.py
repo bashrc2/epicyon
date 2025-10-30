8be5878dba10fbcc46474262f344e0bd8508e7fd
Bob Mottram <bob@libreserver.org>
2025-10-30 17:09:54 +0000

    Less space

--- webapp_profile.py
+++ webapp_profile.py
@@ -3345,7 +3345,7 @@ def _html_edit_profile_main(base_dir: str, display_nickname: str,
 
     max_profile_posts = \
         get_max_profile_posts(base_dir, nickname, domain, max_recent_posts)
-    edit_profile_form += '<br><br><p>' + \
+    edit_profile_form += '<br><p>' + \
         edit_number_field(translate['Preview posts on profile screen'],
                           'maxRecentProfilePosts', max_profile_posts,
                           1, 20, max_recent_posts) + '</p>\n'
