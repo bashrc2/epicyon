a8ad63bb418f7c7e5b68732eff51b59ff0a48ac0
Bob Mottram <bob@libreserver.org>
2025-12-20 11:48:34 +0000

    Debug

--- newswire.py
+++ newswire.py
@@ -354,9 +354,12 @@ def parse_feed_date(pub_date: str, unique_string_identifier: str) -> str:
         try:
             published_date = date_from_string_format(pub_date2, [date_format])
         except BaseException as exc:
-            if '.' not in pub_date2 and \
+            if 'Z' not in pub_date and \
+               'EST' not in pub_date and \
+               'GMT' not in pub_date and \
+               'UT' not in pub_date and \
                date_format == "%a, %d %b %Y %H:%M:%S":
-                errmsg = ' ' + str(exc)
+                errmsg = '| ' + pub_date2 + '| ' + str(exc)
             continue
 
         if published_date:
