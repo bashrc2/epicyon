b62ebad9d1a069d6cc3cc2323ec8a263d402b070
bashrc <bob@libreserver.org>
2026-01-13 10:12:35 +0000

    More suspicious headers

--- httpheaders.py
+++ httpheaders.py
@@ -274,7 +274,7 @@ def contains_suspicious_headers(headers: {}) -> bool:
        'Think-lang' in headers:
         return True
     headers_str = str(headers)
-    sus_strings = ('../../', 'index.php', 'passwd')
+    sus_strings = ('../../', '.php/', 'index.php', 'passwd')
     if string_contains(headers_str, sus_strings):
         return True
     return False
