372753db5cb629cc5edd78d9f0d2905d8d173434
bashrc <bob@libreserver.org>
2026-02-05 22:48:07 +0000

    More bad strings

--- utils.py
+++ utils.py
@@ -3991,7 +3991,8 @@ def check_bad_path(path: str):
 
     bad_strings = ('..', '/.', '%2e%2e', '%252e%252e',
                    '/sftp.', '/sftp-', '/statistics',
-                   '/config/', 'settings.', 'credentials')
+                   '/config/', 'settings.', 'credentials',
+                   '/packs/', '/backend/', '/apis/')
 
     # allow /.well-known/...
     if '/.' in path_lower:
