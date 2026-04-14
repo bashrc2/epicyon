259d51d00f6e2aa0ef85ac26e8f5067c7ddb5b22
bashrc <bob@libreserver.org>
2026-04-14 20:20:27 +0100

    Extra llm crawlers

--- crawlers.py
+++ crawlers.py
@@ -167,7 +167,7 @@ def blocked_user_agent(calling_domain: str, agent_str: str,
             'openai', 'panscient', 'phindbot', 'qualifiedbot', 'quillbot',
             'sbIntuitionsbot', 'semrushbot', 'shapbot', 'terracotta',
             'velenpublicwebcrawler', 'wpbot', 'yak', 'amzn-', 'azureai',
-            'kagi-fetcher'
+            'kagi-fetcher', 'aranet-', 'apifybot', 'apifyweb', 'exabot'
         )
         for bot_str in llm_bot_strings:
             if bot_str in agent_str_lower:
