5f358a1005d23a6db6e6a359e3eeb60e8c7ab1d7
bashrc <bob@libreserver.org>
2026-06-29 10:59:14 +0100

    More LLM crawlers

--- src/crawlers.py
+++ src/crawlers.py
@@ -163,7 +163,9 @@ def blocked_user_agent(calling_domain: str, agent_str: str,
             'sbIntuitionsbot', 'semrushbot', 'shapbot', 'terracotta',
             'terra cotta', 'velenpublicwebcrawler', 'wpbot', 'yak', 'amzn-',
             'azureai', 'kagi-fetcher', 'aranet-', 'apifybot', 'apifyweb',
-            'exabot', 'nagetbot', 'newsai', 'henkbot', 'opencode', 'trae'
+            'exabot', 'nagetbot', 'newsai', 'henkbot', 'opencode', 'trae',
+            'agenttimes', 'shap-user', 'cragcrawler', 'geisthaus-pagefetcher',
+            'kimi-user', 'querit-searchbot', 'useai', 'cursor'
         )
         for bot_str in llm_bot_strings:
             if bot_str in agent_str_lower:
