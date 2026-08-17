c855fbb0342be6d8db9aa62d8b1ff57e22751ec0
bashrc <bob@libreserver.org>
2026-08-17 13:32:09 +0100

    Tidying

--- src/utils.py
+++ src/utils.py
@@ -1474,23 +1474,19 @@ def get_nickname_from_actor(actor: str) -> str:
         nick_str: str = actor.split(possible_path)[1].replace('@', '')
         if '/' not in nick_str:
             nick_str = _remove_did_prefix(nick_str)
-            nick_str = _tidy_nickname(nick_str)
-            return nick_str
+            return _tidy_nickname(nick_str)
         nick_str = nick_str.split('/')[0]
-        nick_str = _tidy_nickname(nick_str)
-        return nick_str
+        return _tidy_nickname(nick_str)
     if '/@/' not in actor:
         if '/@' in actor:
             # https://domain/@nick
             nick_str: str = actor.split('/@')[1]
             if '/' in nick_str:
                 nick_str = nick_str.split('/')[0]
-            nick_str = _tidy_nickname(nick_str)
-            return nick_str
+            return _tidy_nickname(nick_str)
         if '@' in actor:
             nick_str: str = actor.split('@')[0]
-            nick_str = _tidy_nickname(nick_str)
-            return nick_str
+            return _tidy_nickname(nick_str)
     if '://' in actor:
         domain: str = actor.split('://')[1]
         if '/' in domain:
@@ -1500,8 +1496,7 @@ def get_nickname_from_actor(actor: str) -> str:
         nick_str: str = actor.split('://' + domain + '/')[1]
         if '/' in nick_str or '.' in nick_str:
             return None
-        nick_str = _tidy_nickname(nick_str)
-        return nick_str
+        return _tidy_nickname(nick_str)
     return None
 
 
